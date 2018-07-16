7ae7588265e27ff0f21e63ddb6bbb290a82d9418
Thanh Nguyen <thanh.nguyen@asnet.com.vn>
2018-07-16 21:51:33 +0700

    fix prototype of id to string required

--- Idiomatic-Redux/src/loadState.js
+++ Idiomatic-Redux/src/loadState.js
@@ -11,10 +11,12 @@ export const getState = () => {
 }
 
 export const setState = (state) => {
+  console.log('testing setState 1', state);
   try {
     const serializedState = JSON.stringify(state)
     localStorage.setItem('state', serializedState)
   } catch (e) {
     console.log('ERROR SET STATE', e)
   }
+  console.log('testing setState 2');
 }
